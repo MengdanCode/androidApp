00fefc1e38af2141c5f5ef0e50817ddd919b86e4
Mengdan <zhaomengdan93@gmail.com>
2014-10-12 18:23:34 +0800

    activity changed

--- src/com/example/ifinternalapp/HomePage.java
+++
@@ -1,7 +0,0 @@
-package com.example.ifinternalapp;
-
-import android.support.v7.app.ActionBarActivity;
-
-public class HomePage extends ActionBarActivity {
-
-}
--- src/com/example/ifinternalapp/HomePageActivity.java
+++ src/com/example/ifinternalapp/HomePageActivity.java
@@ -5,18 +5,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainMenu2 extends ActionBarActivity {
+public class HomePageActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main_menu2);
+		setContentView(R.layout.activity_home_page);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main_menu2, menu);
+		getMenuInflater().inflate(R.menu.home_page, menu);
 		return true;
 	}
 
